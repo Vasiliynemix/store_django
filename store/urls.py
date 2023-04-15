3d4ebc875e6c48933e3a8b86a721f1a5f29a1b15
Vasiliy Zuev <nemixvasya@gmail.com>
2023-04-15 17:41:31 +0300

    add urls in products

--- store/urls.py
+++ store/urls.py
@@ -14,17 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from products.views import index, products
+from products.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('products/', products, name='products'),
+    path('products/', include('products.urls', namespace='products')),
 ]
 
 if settings.DEBUG:

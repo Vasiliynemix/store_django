6cdc50edf77ff223a994246089c6213e9337fd8c
Vasiliy Zuev <nemixvasya@gmail.com>
2023-04-16 21:30:36 +0300

    add basket(hard for me)

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 
-from users.models import User
 from users.froms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from products.models import Basket
 
 
 # Create your views here.
@@ -40,6 +41,7 @@ def registration(request):
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
@@ -48,9 +50,11 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
+
     context = {
         'title': 'Store - Профиль',
         'form': form,
+        'baskets': Basket.objects.filter(user=request.user),
     }
     return render(request, 'users/profile.html', context)
 

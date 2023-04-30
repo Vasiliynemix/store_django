1f099664fed3b08c436ed62c00d63ab6d86fe525
Vasiliy Zuev <nemixvasya@gmail.com>
2023-04-30 20:42:14 +0300

    add OrderDetailView

--- orders/views.py
+++ orders/views.py
@@ -1,18 +1,19 @@
-import stripe
 from http import HTTPStatus
 
+import stripe
 from django.conf import settings
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import TemplateView
-from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
-from products.models import Basket
 from orders.models import Order
+from products.models import Basket
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -35,6 +36,16 @@ class OrderListView(TitleMixin, ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         return queryset.filter(initiator=self.request.user)
+
+
+class OrderDetailView(DetailView):
+    template_name = 'orders/order.html'
+    model = Order
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = f'Store - Заказ #{self.object.id}'
+        return context
 
 
 class OrderCreateView(TitleMixin, CreateView):

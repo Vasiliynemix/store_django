9d0932a5a4d9a67e5a3008907ac4a90b6d896b5b
Vasiliy Zuev <nemixvasya@gmail.com>
2023-04-30 20:22:20 +0300

    add OrderListView

--- orders/urls.py
+++ orders/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from orders.views import OrderCreateView, CanceledTemplateView, SuccessTemplateView
+from orders.views import OrderCreateView, CanceledTemplateView, SuccessTemplateView, OrderListView
 app_name = 'orders'
 
 urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name='order_create'),
+    path('', OrderListView.as_view(), name='orders_list'),
     path('order-success/', SuccessTemplateView.as_view(), name='order_success'),
     path('order_canceled/', CanceledTemplateView.as_view(), name='order_canceled'),
 ]
